7955a3117c3ec957a5dfbb61a35b30f3754350a6
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-10-14 18:19:58 +0200

    walking enemy attack method

--- src/entity/enemy/walkingEnemy.py
+++ src/entity/enemy/walkingEnemy.py
@@ -24,6 +24,7 @@ class WalkingEnemy(Entity):
             self.frameIndex = 0
             
         self.__moveCloserToPlayer(player)
+        self.__attackPlayer(surface, player)
             
     def __moveCloserToPlayer(self, player):
         if(player.body.centerx > self.body.centerx):
@@ -32,3 +33,12 @@ class WalkingEnemy(Entity):
         elif(player.body.centerx < self.body.centerx):
             self.body.x -= walkingEnemyConfig["SPEED"]
             self.flip = True
+            
+    def __attackPlayer(self, surface, player):
+        attackRange = pygame.Rect(self.body.left if self.flip else self.body.right, self.body.top+40, walkingEnemyConfig["ATTACK_WIDTH"], self.body.height-40)
+        pygame.draw.rect(surface, (0,255,0), attackRange)
+        
+        if(attackRange.colliderect(player.body)):
+            player.health -= walkingEnemyConfig["DAMAGE"]
+            player.hit = True
+            player.body.x += -walkingEnemyConfig["ATTACK_WIDTH"] if self.flip else walkingEnemyConfig["ATTACK_WIDTH"]
